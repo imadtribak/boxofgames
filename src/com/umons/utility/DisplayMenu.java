41d9198a237f247f821829aad2a0bca479517f2f
AGOZZINO Terencio <agozzino.terencio@gmail.com>
2014-03-08 12:24:38 +0100

    AGOZZINO Terencio <agozzino.terencio@gmail.com>

--- src/com/umons/utility/DisplayMenu.java
+++ src/com/umons/utility/DisplayMenu.java
@@ -32,47 +32,53 @@ public class DisplayMenu
 	
 	public static void printMenu()
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Select the game you want to play:\n");
-		System.out.println("1. Tic-Tac-Toe\n");
-		System.out.println("2. Four-In-A-Line\n");
-		System.out.println("3. Othello\n");
-		System.out.println("4. Quit\n");
-		int choice = sc.nextInt();
-
-		while (choice > 0 && choice < 5)
+		try
 		{
-			System.out.println("Select the game you want to play:\n");
+			Scanner sc = new Scanner(System.in);
 			System.out.println("1. Tic-Tac-Toe\n");
 			System.out.println("2. Four-In-A-Line\n");
 			System.out.println("3. Othello\n");
 			System.out.println("4. Quit\n");
-			choice = sc.nextInt();
-		}
-			switch (choice)
-			{
-				case 1:
-					TicTacToe tic_tac_toe = new TicTacToe();
-					System.out.println(tic_tac_toe.getGameTable());
-					break;
+			int choice = sc.nextInt();
+            
+    		while (true)
+    		{
+				switch (choice)
+				{
+					case 1:
+						TicTacToe tic_tac_toe = new TicTacToe();
+						System.out.println(tic_tac_toe.getGameTable());
+						break;
+						
+					case 2:
+						FourInARow four_in_a_line = new FourInARow();
+						System.out.println(four_in_a_line.getGameTable());
+						break;
+							
+					case 3:
+						Othello othello = new Othello();
+						System.out.println(othello.getGameTable());
+						break;
+			
+					case 4:
+						System.exit(0);
+						break;
+							
+					default:
+						System.out.println("Please, choose a number between 1 and 4\n");
+						printMenu();
+				}
 				
-				case 2:
-					FourInARow four_in_a_line = new FourInARow();
-					System.out.println(four_in_a_line.getGameTable());
-					break;
-					
-				case 3:
-					Othello othello = new Othello();
-					System.out.println(othello.getGameTable());
-					break;
-	
-				case 4:
-					System.exit(0);
-					break;
-					
-				default:
-					System.out.println("Please, try again");
-			}
-		sc.close();
+			break;
+    		}
+    		
+			sc.close();
+		}
+		
+		catch (Exception ex)
+		{
+			System.out.println("ERROR: Invalid Type - Please, enter an integer number\n");
+			printMenu();
+		}
 	}
 }
